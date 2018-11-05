4254fa464f9bd5b0e88bfbdc07ccefea761c149d
phdstudent15030049 <15030049@lums.edu.pk.com>
2018-11-05 12:45:28 +0500

    committing the one line change of nodeenv apioptions server

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -3,7 +3,7 @@ var apiOptions = {
   server: "http://localhost:3000"
 };
 if (process.env.NODE_ENV === "production") {
-  apiOptions.server = "https://loc8rdemo-2018.herokuapp.com";
+  apiOptions.server = "https://loc8r-session.herokuapp.com";
 }
 
 var mongoose = require("mongoose");
